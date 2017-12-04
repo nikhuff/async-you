750e29e01d81613be8725fc71422fca30b73a24a
nikhuff <nik.huff@me.com>
2017-12-04 14:50:47 -0700

    finished series solution

---
+++ series.js
@@ -0,0 +1,28 @@
+var async = require('async'),
+    http = require('http');
+
+async.series({
+    requestOne: function (done) {
+        fetchUrl(process.argv[2], done)
+    },
+    requestTwo: function (done) {
+        fetchUrl(process.argv[3], done);
+    }
+}, function (err, results) {
+    if (err) console.error(err);
+    console.log(results);
+});
+
+function fetchUrl(url, done) {
+    var body = '';
+    http.get(url, function (res) {
+        res.on('data', function (chunk) {
+            body += chunk.toString();
+        });
+        res.on('end', function () {
+            done(null, body);
+        });
+    }).on('error', function (err) {
+        done(err);
+    });
+}
